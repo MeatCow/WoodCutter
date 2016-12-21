06f11fe060ad83264907931652cb68765a4b8d56
andy2great <great2g@hotmail.com>
2016-12-21 18:05:40 -0500

    Ajout d' une methode pour tourner la camera aleatoirement

--- src/scripts/WoodCutter.java
+++ src/scripts/WoodCutter.java
@@ -1,8 +1,9 @@
 package scripts;
 
-import org.powerbot.script.PollingScript;
-import org.powerbot.script.Script;
+import org.powerbot.script.*;
 import org.powerbot.script.rt4.ClientContext;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Script.Manifest(
         name = "Hello, RSBot!", properties = "author=Coma; topic=1296203; client=4;",
@@ -12,11 +13,27 @@ public class WoodCutter extends PollingScript<ClientContext> {
 
     @Override
     public void start() {
-        log.info("Hello, RSBot!");
+
+        this.stop();
     }
 
     @Override
     public void poll() {
 
+    }
+
+    /**
+     * Methode qui permet tourne la camera dans une position aleatoire
+     */
+    private void turnCameraRandomPosition(){
+        int x = ThreadLocalRandom.current().nextInt(-10000, 10000);
+        int y = ThreadLocalRandom.current().nextInt(-10000, 10000);
+        int z = ThreadLocalRandom.current().nextInt(-10000, 10000);
+        ctx.camera.turnTo(new Locatable() {
+            @Override
+            public Tile tile() {
+                return new Tile(x,y,z);
+            }
+        });
     }
 }
