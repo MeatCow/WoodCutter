38a439e1bf7785dd377054e9b4f83178907a1a30
Matthieu <matt.pauze@gmail.com>
2016-12-20 22:12:55 -0500

    Initial Commit

---
+++ src/Chop.java
@@ -0,0 +1,25 @@
+import org.powerbot.script.rt4.ClientContext;
+
+/**
+ * Created by Matt on 20/12/2016.
+ */
+public class Chop extends Task<ClientContext> {
+
+    public Chop(ClientContext ctx) {
+        super(ctx);
+    }
+
+    @Override
+    public boolean activate() {
+        boolean shouldActivate = true;
+        if (true) {
+
+        }
+        return shouldActivate;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ src/PowerChopper.java
@@ -0,0 +1,16 @@
+import org.powerbot.script.PollingScript;
+
+/**
+ * Created by Matt on 20/12/2016.
+ */
+public class PowerChopper extends PollingScript {
+
+    public void start() {
+
+    }
+
+
+    public void poll() {
+
+    }
+}
