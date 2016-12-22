b8e53959147e2a4a0afaddc8cfbde2a808443b2f
andy2great <great2g@hotmail.com>
2016-12-22 13:05:17 -0500

    Add new random security to chopchop Add new get position secutirty to chopchop

--- src/scripts/WoodCutter.java
+++ src/scripts/WoodCutter.java
@@ -1,11 +1,12 @@
 package scripts;
 
-import com.sun.javafx.geom.Area;
+import org.powerbot.script.Locatable;
 import org.powerbot.script.PollingScript;
 import org.powerbot.script.Script;
 import org.powerbot.script.Tile;
 import org.powerbot.script.rt4.*;
-import java.util.List;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Script.Manifest(
         name = "WoodCutterXtreme", properties = "author=Andrew & Matthieu; client=4;",
@@ -16,15 +17,16 @@ public class WoodCutter extends PollingScript<ClientContext> {
     private BasicQuery<GameObject> allTrees;
     private final int DOING_NOTHING = -1;
     private final int RADIUS = 15;
+    private final int WALKING_DISTANCE = 2;
     private int[] cutable = {1278,1276,1751};
     private int[] dropable = {1511,1521};
-
     private Tile initialTile;
 
     @Override
     public void start() {
         allTrees = ctx.objects.select().id(cutable);
         initialTile = ctx.players.local().tile();
+
     }
 
     @Override
@@ -38,8 +40,10 @@ public class WoodCutter extends PollingScript<ClientContext> {
                 ctx.movement.step(initialTile);
             } else {
                 GameObject tree = allTrees.nearest().poll();
-                ctx.camera.turnTo(tree);
-                ctx.movement.step(tree);
+                ctx.camera.turnTo(getTreePositionRandom(tree.tile()));
+                if (treeFarAway(tree)){
+                    ctx.movement.step(tree);
+                }
                 tree.interact("Chop");
             }
         }
@@ -69,4 +73,29 @@ public class WoodCutter extends PollingScript<ClientContext> {
     private boolean inventoryIsFull() {
         return ctx.inventory.select().count() == 28;
     }
+
+    private Boolean treeFarAway(GameObject tree){
+        int playerLocationX = ctx.players.local().tile().x();
+        int playerLocationY = ctx.players.local().tile().y();
+        int treeLocationX = tree.tile().x();
+        int treeLocationY = tree.tile().y();
+        return  playerLocationX < treeLocationX - WALKING_DISTANCE
+                || playerLocationX > treeLocationX + WALKING_DISTANCE
+                || playerLocationY < treeLocationY - WALKING_DISTANCE
+                || playerLocationY > treeLocationY + WALKING_DISTANCE;
+    }
+
+    public Locatable getTreePositionRandom(Tile treePosition) {
+        int randomPositionX = (treePosition.x() - 2) + getRandomInt(5);
+        int randomPositionY = (treePosition.y() - 2) + getRandomInt(5);
+        Tile tile = new Tile(randomPositionX, randomPositionY);
+        return tile;
+    }
+
+    private int getRandomInt(int maxInt){
+        return ThreadLocalRandom.current().nextInt(0, maxInt + 1);
+    }
+    /*private Objects checkInventory(){
+
+    }*/
 }
