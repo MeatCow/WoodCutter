0293a89919b5339a792472f177e4c9b6cdbde184
Matthieu <matt.pauze@gmail.com>
2016-12-22 14:33:53 -0500

    Now walks using minimap only if tree is too far away

--- src/scripts/WoodCutter.java
+++ src/scripts/WoodCutter.java
@@ -18,8 +18,8 @@ public class WoodCutter extends PollingScript<ClientContext> {
     private final int DOING_NOTHING = -1;
     private final int RADIUS = 15;
     private final int WALKING_DISTANCE = 2;
-    private int[] cutable = {1278, 1276, 1751};
-    private int[] dropable = {1511, 1521};
+    private int[] cutable = {1278,1276,1751};
+    private int[] dropable = {1511,1521};
     private Tile initialTile;
 
     @Override
@@ -60,7 +60,7 @@ public class WoodCutter extends PollingScript<ClientContext> {
     }
 
     private void dropAllLogs() {
-        for (Item inventoryItem : ctx.inventory.select().id(dropable)) {
+        for (Item inventoryItem: ctx.inventory.select().id(dropable)) {
             inventoryItem.interact("Drop");
         }
     }
@@ -94,7 +94,7 @@ public class WoodCutter extends PollingScript<ClientContext> {
         return tile;
     }
 
-    private int getRandomInt(int maxInt) {
+    private int getRandomInt(int maxInt){
         return ThreadLocalRandom.current().nextInt(0, maxInt + 1);
     }
     /*private Objects checkInventory(){
