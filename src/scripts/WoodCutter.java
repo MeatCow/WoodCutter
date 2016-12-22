52f0cb4621b01a8b595f36826956c62211268d0f
andy2great <great2g@hotmail.com>
2016-12-22 01:06:10 -0500

    Maintenant le chopchop peut chopchop les oaks tree et drop tout se qui a ete chopchop
    Le radius a ete implenter dans le cancer

--- src/scripts/WoodCutter.java
+++ src/scripts/WoodCutter.java
@@ -1,12 +1,10 @@
 package scripts;
 
+import com.sun.javafx.geom.Area;
 import org.powerbot.script.PollingScript;
 import org.powerbot.script.Script;
+import org.powerbot.script.Tile;
 import org.powerbot.script.rt4.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Script.Manifest(
@@ -15,45 +13,60 @@ import java.util.List;
 )
 public class WoodCutter extends PollingScript<ClientContext> {
 
-    List<GameObject> trees;
-    private int[] cutable = {1278,1276};
+    private BasicQuery<GameObject> allTrees;
+    private final int DOING_NOTHING = -1;
+    private final int RADIUS = 15;
+    private int[] cutable = {1278,1276,1751};
+    private int[] dropable = {1511,1521};
+
+    private Tile initialTile;
 
     @Override
     public void start() {
+        allTrees = ctx.objects.select().id(cutable);
+        initialTile = ctx.players.local().tile();
     }
 
     @Override
     public void poll() {
-        if (shouldCut()) {
-            GameObject tree = ctx.objects.select().id(cutable).nearest().poll();
-            ctx.camera.turnTo(tree);
-            ctx.movement.step(tree);
-            tree.interact("Chop");
-        }
-        else if (inventoryIsFull()) {
-            dropAllLogs();
-        }
-    }
+        allTrees = ctx.objects.select().id(cutable);
 
-    private void dropAllLogs() {
-        for (Item inventoryItem: ctx.inventory.items()) {
-            if (inventoryItem.id() == 1511) {
-                inventoryItem.interact("Drop");
+        if (inventoryIsFull()) {
+            dropAllLogs();
+        } else if (shouldCut()) {
+            if (shouldStepBack()) {
+                ctx.movement.step(initialTile);
+            } else {
+                GameObject tree = allTrees.nearest().poll();
+                ctx.camera.turnTo(tree);
+                ctx.movement.step(tree);
+                tree.interact("Chop");
             }
         }
     }
 
-    private boolean shouldCut() {
-        if (!inventoryIsFull()
-                && ctx.players.local().animation() == -1
-                && !ctx.players.local().inMotion()) {
-            return true;
+    private boolean shouldStepBack() {
+        int locationX = ctx.players.local().tile().x();
+        int locationY = ctx.players.local().tile().y();
+        return locationX < initialTile.x() - RADIUS
+                || locationX > initialTile.x() + RADIUS
+                || locationY < initialTile.y() - RADIUS
+                || locationY > initialTile.y() + RADIUS;
+    }
+
+    private void dropAllLogs() {
+        for (Item inventoryItem: ctx.inventory.select().id(dropable)) {
+            inventoryItem.interact("Drop");
         }
-        return false;
+    }
+
+    private boolean shouldCut() {
+        return !inventoryIsFull()
+                && ctx.players.local().animation() == DOING_NOTHING
+                && !ctx.players.local().inMotion();
     }
 
     private boolean inventoryIsFull() {
-        return ctx.inventory.size() == 28;
+        return ctx.inventory.select().count() == 28;
     }
-
 }
